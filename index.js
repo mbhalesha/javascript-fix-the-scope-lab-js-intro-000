5ce2fee3d000fbca8fe26add5dd302f082548f02
Manisha Bhalesha <mbhalesha@gmail.com>
2018-06-09 22:06:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function yourAnimal() {
   return animal
 }
 
-function add2(70) {
-  return n + two
+function add2(n) {
+  return 70 + two
 
   // Feel free to move things around!
   const two = 2
